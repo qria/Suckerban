3510d51b2837e554ad2e5629e49f267cf72c694e
qria <frigen@naver.com>
2016-09-29 14:39:44 +0900

    Fancy game over screen

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,10 +6,12 @@ public class LevelManager : MonoBehaviour
     private List<SuckerbanObject> allObjects = new List<SuckerbanObject>();
     private AudioSource audio;
     public Player currentPlayer;
+    public GameObject gameOverScreen;
 
     void Awake()
     {
         audio = GetComponent<AudioSource>();
+        gameOverScreen.SetActive(false);
     }
     
 	void Update () {
@@ -52,6 +54,7 @@ public class LevelManager : MonoBehaviour
     {
         Debug.Log("YOU DIED");
         audio.Play();
+        gameOverScreen.SetActive(true);
         Destroy(currentPlayer.transform.gameObject);
     }
 }

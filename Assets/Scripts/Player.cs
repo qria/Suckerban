f9dc6e1132bd53cbc98a92599679096bb132d614
qria <frigen@naver.com>
2016-11-03 14:45:49 +0900

    Apply collider for player death

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 using System.Collections.Generic;
 using Object = UnityEngine.Object;
 
-public class Player : SuckerbanObject {
+public class Player : SuckerbanObject
+{
+    private BoxCollider2D collider;
     void Awake () {
         transform = GetComponent<Transform>();
-        level = Object.FindObjectOfType<LevelManager>();
+        level = FindObjectOfType<LevelManager>();
         level.Add(this);
         level.currentPlayer = this;
+
+        collider = GetComponent<BoxCollider2D>();
     }
 	
 	void Update ()
@@ -18,30 +22,8 @@ public class Player : SuckerbanObject {
 	    {
 	        if (Input.GetKeyDown(direction.GetKeyCode()))
 	        {
-	            push(direction);
+	            move(direction);
 	        }
 	    }
 	}
-
-    public override void push(Direction direction, List<SuckerbanObject> alreadyPushedObjects = null) {
-
-        alreadyPushedObjects = alreadyPushedObjects ?? new List<SuckerbanObject>(); // Dynamic parameter 
-        // Use alreadyPushedObjects to circumvent recursions
-        if (alreadyPushedObjects.Contains(this)) {
-            return;
-        }
-        alreadyPushedObjects.Add(this);
-
-        SuckerbanObject objInFront = level.GetObjectInPosition(transform.position + direction.GetVector());
-        if (objInFront != null && objInFront != this)
-        {
-            if (direction == Direction.Down && objInFront is Triangle)
-            {
-                level.gameOver();
-                return;
-            }
-            objInFront.push(direction, alreadyPushedObjects);
-        }
-        move(direction);
-    }
 }
--- Assets/Scripts/Triangle.cs
+++ Assets/Scripts/Triangle.cs
@@ -2,9 +2,20 @@
 using System.Collections.Generic;
 
 public class Triangle : SuckerbanObject {
+    private BoxCollider2D collider;
+
     void Awake() {
         transform = GetComponent<Transform>();
         level = FindObjectOfType<LevelManager>();
         level.Add(this);
+
+        collider = GetComponent<BoxCollider2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player")){
+            level.gameOver();
+        }
     }
 }

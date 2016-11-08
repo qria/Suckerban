e2d8235c3ca0af9747adac97a5c1c9646b26c3b6
qria <frigen@naver.com>
2016-11-08 15:56:18 +0900

    New Moving algorithm

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,12 +8,6 @@ public class Player : SuckerbanObject
     // Components
     private BoxCollider2D collider;
 
-    // For movements
-    private bool isMoving = false;
-    private float remainingMoveDistance;
-    private float moveSpeed = 10f;
-    private Direction moveDirection;
-    
     // Constants 
     private Array AllDirections = Enum.GetValues(typeof(Direction));
 
@@ -26,32 +20,14 @@ public class Player : SuckerbanObject
         collider = GetComponent<BoxCollider2D>();
     }
 	
-	void Update ()
+	protected override void UpdateInput ()
     {
-	    if (isMoving)
-	    {
-	        float moveDistance = moveSpeed * Time.deltaTime; // move distance for current frame
-            Debug.Log(moveDistance);
-	        if (moveDistance >= remainingMoveDistance)
-	        {
-	            moveDistance = remainingMoveDistance;
-	        }
-	        move(moveDirection, moveDistance);
-	        remainingMoveDistance -= moveDistance;
-	        if (remainingMoveDistance < 0.000000001)
-	        {
-	            isMoving = false;
-	        }
-	    }
-	    else {
-            foreach (Direction direction in AllDirections) {
-                if (Input.GetKey(direction.GetKeyCode())) // Autofire because delay is there
-                {
-                    isMoving = true;
-                    moveDirection = direction;
-                    remainingMoveDistance = 1f;
-                }
+        foreach (Direction direction in AllDirections) {
+            if (Input.GetKeyDown(direction.GetKeyCode()) && !isMoving) // Autofire because delay is there
+            {
+                push(direction);
             }
         }
+
     }
 }
--- Assets/Scripts/SuckerbanObject.cs
+++ Assets/Scripts/SuckerbanObject.cs
@@ -9,9 +9,61 @@ public class SuckerbanObject : MonoBehaviour {
     protected LevelManager level;
     public List<Vector2> localPositions; // If this object is spanned
     // Note that this is relative position to avoid uncessary updates
+    public List<Vector2> positions {
+        get {
+            if (localPositions.Count <= 1) {
+                return new List<Vector2>() {transform.position};
+            }
+            // For Walls
+            // This algorithm breaks down when multiple surface is juxtaposed
+            return localPositions.ConvertAll(x => (Vector2) transform.position + x);
+        }
+    }
+    
+    // For movements
+    protected bool isMoving = false;
+    protected float remainingMoveDistance;
+    public float moveSpeed = 6f;
+    protected Direction moveDirection;
+    
+    void Update() {
+        // Broke down Update() into multiple methods
+        // It is encouraged not to use  Update()` in children class
+        UpdateMove();
+        UpdateInput();
+    }
 
-    public void move(Direction direction, float size=1) {
-        transform.position += direction.GetVector() * size;
+    protected virtual void UpdateMove() {
+        // Update function that handles movements
+
+        if (!isMoving) { // Only continue when moving
+            return;
+        }
+
+        float moveDistance = moveSpeed * Time.deltaTime; // move distance for current frame
+        if (moveDistance >= remainingMoveDistance) {
+            moveDistance = remainingMoveDistance;
+        }
+        move(moveDirection, moveDistance);
+        remainingMoveDistance -= moveDistance;
+        if (remainingMoveDistance < 0.000000001) {
+            isMoving = false;
+        }
+    }
+
+    protected virtual void UpdateInput() {
+        // Update function that handles inputs
+    }
+
+
+    protected void startMoving(Direction direction, float distance=1f) {
+        isMoving = true;
+        remainingMoveDistance = distance;
+        moveDirection = direction;
+    }
+
+    public void move(Direction direction, float distance=1f) {
+        transform.position += direction.GetVector() * distance;
     }
     
     public virtual void push(Direction direction, List<SuckerbanObject> alreadyPushedObjects=null) {
@@ -19,35 +71,27 @@ public class SuckerbanObject : MonoBehaviour {
         // move just moves it and push propagates it
 
         alreadyPushedObjects = alreadyPushedObjects ?? new List<SuckerbanObject>(); // Dynamic parameter 
+
         // Use alreadyPushedObjects to circumvent recursions
         if (alreadyPushedObjects.Contains(this)) {
             return;
         }
         alreadyPushedObjects.Add(this);
         
-        if (localPositions.Count <= 1) {
-            SuckerbanObject objInFront = level.GetObjectInPosition(transform.position + direction.GetVector());
-            if (objInFront != null && objInFront != this)
-            {
+        
+        foreach (Vector2 position in positions)
+        {
+            Vector2 positionInFront = position + (Vector2)direction.GetVector();
+            SuckerbanObject objInFront = level.GetObjectInPosition(positionInFront);
+            if (objInFront != null && objInFront != this) {
+                if (direction == Direction.Down && (this is Player) && (objInFront is Triangle)) { 
+                    // If facing spike can't push
+                    continue;
+                }
                 objInFront.push(direction, alreadyPushedObjects);
             }
-            move(direction);
         }
-        else
-        {
-            // For Walls
-            // This algorithm breaks down when multiple surface is juxtaposed
-            foreach (Vector2 localPosition in localPositions)
-            {
-                Vector2 positionInFront = (Vector2) transform.position + localPosition + (Vector2)direction.GetVector();
-                SuckerbanObject objInFront = level.GetObjectInPosition(positionInFront);
-                if (objInFront != null && objInFront != this) {
-                    objInFront.push(direction, alreadyPushedObjects);
-                }
-
-            }
-            move(direction);
-        }
+        startMoving(direction);
 
     }
 }

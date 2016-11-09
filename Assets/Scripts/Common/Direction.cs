ad0f4e1e0dda6acacb8e5c492314d6e2534711e1
Qria <qria@qria.net>
2016-11-10 00:27:51 +0900

    .gitignore doesn't ignore my scripts anymore

---
+++ Assets/Scripts/Common/Direction.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public enum Direction {
+    Up, Down, Left, Right
+}
+
+static class DirectionMethods {
+    /* Extension class for Direction */
+
+    // Unfortunately this has to use a getter since there is no extension property in csharp
+    // so no direction.keyCode but only direction.GetKeyCode()
+
+    public static Dictionary<Direction, KeyCode> directionToKeyCodeDictionary =
+        new Dictionary<Direction, KeyCode>()
+        {
+            {Direction.Up, KeyCode.UpArrow},
+            {Direction.Down, KeyCode.DownArrow},
+            {Direction.Right, KeyCode.RightArrow},
+            {Direction.Left, KeyCode.LeftArrow}
+        };
+
+    public static Dictionary<KeyCode, Direction> KeyCodeToDirectionDictionary =
+        new Dictionary<KeyCode, Direction>()
+        {
+            {KeyCode.UpArrow, Direction.Up},
+            {KeyCode.DownArrow, Direction.Down},
+            {KeyCode.RightArrow, Direction.Right},
+            {KeyCode.LeftArrow, Direction.Left}
+        };
+
+    public static Dictionary<Direction, Vector3> directionToVectorDictionary =
+        new Dictionary<Direction, Vector3>()
+        {
+            {Direction.Up, Vector3.up},
+            {Direction.Down, Vector3.down},
+            {Direction.Right, Vector3.right},
+            {Direction.Left, Vector3.left}
+        };
+
+    public static Dictionary<Direction, IntVector2> directionToIntVector2Dictionary =
+        new Dictionary<Direction, IntVector2>()
+        {
+            {Direction.Up, IntVector2.up},
+            {Direction.Down, IntVector2.down},
+            {Direction.Right, IntVector2.right},
+            {Direction.Left, IntVector2.left}
+        };
+
+
+    public static KeyCode GetKeyCode(this Direction direction) {
+        return directionToKeyCodeDictionary[direction];
+    }
+
+    public static Direction GetDirection(this KeyCode keyCode)
+    {
+        return KeyCodeToDirectionDictionary[keyCode];
+    }
+
+    public static Vector3 GetVector(this Direction direction) {
+        return directionToVectorDictionary[direction];
+    }
+
+    public static IntVector2 GetIntVector2(this Direction direction) {
+        return directionToIntVector2Dictionary[direction];
+    }
+}
---
+++ Assets/Scripts/Common/IntVector2.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public struct IntVector2 {
+    public int x, y;
+
+    public IntVector2(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public override string ToString() {
+        return string.Format("({0},{1})", x, y);
+    }
+
+    public static IntVector2 up = new IntVector2(0, 1);
+    public static IntVector2 down = new IntVector2(0, -1);
+    public static IntVector2 right = new IntVector2(1, 0);
+    public static IntVector2 left = new IntVector2(-1, 0);
+
+    public static IntVector2 operator +(IntVector2 v1, IntVector2 v2) {
+        return new IntVector2(v1.x + v2.x, v1.y + v2.y);
+    }
+}
+
+public static class IntVector2Extensions {
+    public static Vector2 ToVector2(this IntVector2 intVector2) {
+        return new Vector2(intVector2.x, intVector2.y);
+    }
+
+    public static IntVector2 ToIntVector2(this Vector3 vector) {
+        return new IntVector2((int)vector.x, (int)vector.y);
+    }
+}

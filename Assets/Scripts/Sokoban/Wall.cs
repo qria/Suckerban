03cc8fe61bfe00d9e7ca3f387026fc7f0ef6aab1
Qria <qria@qria.net>
2016-11-09 21:00:17 +0900

    Refactoring continued

---
+++ Assets/Scripts/Sokoban/Wall.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Wall : SuckerbanObject{
+
+    protected override void AwakeInitialize() {
+        transform = GetComponent<Transform>();
+        level = FindObjectOfType<LevelManager>();
+        level.Add(this);
+        foreach (Transform childWall in transform)
+        {
+            localPositions.Add(childWall.transform.localPosition.ToIntVector2());
+        }
+    }
+}

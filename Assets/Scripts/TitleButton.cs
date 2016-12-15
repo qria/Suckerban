ba47c04e76c431a381e868b1190a1fbd9ca69da3
Qria <qria@qria.net>
2016-12-15 09:33:46 +0900

    Title screen

---
+++ Assets/Scripts/TitleButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TitleButton : MonoBehaviour {
+
+    public string firstLevel = "World0Level1";
+    public void startHardGame() {
+        SceneManager.LoadScene(firstLevel);
+    }
+}
